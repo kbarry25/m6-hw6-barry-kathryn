e85ae92a05ef8a0ef8e060f6efdd6de0ac95f68b
Kathryn Barry <kbarry25@ufl.edu>
2020-10-11 23:31:14 -0400

    edited main.js

---
+++ main.js
@@ -0,0 +1,71 @@
+document.getElementById("one").addEventListener("mouseenter",
+  function() {
+    document.getElementById("one").style.backgroundColor = "orange";
+    document.getElementById("two").style.backgroundColor = "orange";
+    document.getElementById("three").style.backgroundColor = "orange";
+  }, false
+);
+
+document.getElementById("two").addEventListener("mouseenter",
+  function() {
+    document.getElementById("one").style.backgroundColor = "orange";
+    document.getElementById("two").style.backgroundColor = "orange";
+    document.getElementById("three").style.backgroundColor = "orange";
+  }, false
+);
+
+document.getElementById("three").addEventListener("mouseenter",
+  function() {
+    document.getElementById("one").style.backgroundColor = "orange";
+    document.getElementById("two").style.backgroundColor = "orange";
+    document.getElementById("three").style.backgroundColor = "orange";
+  }, false
+);
+
+document.getElementById("one").addEventListener("mouseleave",
+  function() {
+    document.getElementById("one").style.backgroundColor = "yellow";
+    document.getElementById("two").style.backgroundColor = "#00ffff";
+    document.getElementById("three").style.backgroundColor = "#00FF00";
+  }, false
+);
+
+document.getElementById("two").addEventListener("mouseleave",
+  function() {
+    document.getElementById("one").style.backgroundColor = "yellow";
+    document.getElementById("two").style.backgroundColor = "#00ffff";
+    document.getElementById("three").style.backgroundColor = "#00FF00";
+  }, false
+);
+
+document.getElementById("three").addEventListener("mouseleave",
+  function() {
+    document.getElementById("one").style.backgroundColor = "yellow";
+    document.getElementById("two").style.backgroundColor = "#00ffff";
+    document.getElementById("three").style.backgroundColor = "#00FF00";
+  }, false
+);
+
+
+document.getElementById("one").addEventListener("click",
+  function() {
+    var newPara = document.createElement("p");
+    newPara.innerHTML = "Oooh - so close, but no cigar";
+    document.getElementById("one").appendChild(newPara);
+  })
+
+
+document.getElementById("two").addEventListener("click",
+  function() {
+    var newPara = document.createElement("p");
+    newPara.innerHTML = "DING DING DING - We have a winner";
+    document.getElementById("two").appendChild(newPara);
+  })
+
+
+document.getElementById("three").addEventListener("click",
+  function() {
+    var newPara = document.createElement("p");
+    newPara.innerHTML = "Oops, butter luck next time";
+    document.getElementById("three").appendChild(newPara);
+  })
